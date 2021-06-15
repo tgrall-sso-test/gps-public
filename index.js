3cd110a93b4d12b26ceecd498dca80c1ca18a3ea
Tugdual Grall <tgrall@github.com>
2021-06-15 11:09:20 +0200

    nbew message #1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var figlet = require('figlet');
  
-figlet('Hello World!!', function(err, data) {
+figlet('Hello World from node!!', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
         console.dir(err);
